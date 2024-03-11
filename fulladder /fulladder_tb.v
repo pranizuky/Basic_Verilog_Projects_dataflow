414b279e8f4ca7f69d341e81bf736f50d84783d9
Pranjal Upadhyay <106436202+pranizuky@users.noreply.github.com>
2024-03-11 10:06:22 +0530

    Update fulladder_tb.v

--- fulladder /fulladder_tb.v
+++ fulladder /fulladder_tb.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09.03.2024 12:10:03
-// Design Name: 
-// Module Name: fulladder_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module fulladder_tb();
