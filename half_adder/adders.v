d85c3da113f6f5112d5380999cf65aa061325299
Pranjal Upadhyay <106436202+pranizuky@users.noreply.github.com>
2024-03-11 09:52:29 +0530

    Update adders.v

--- half_adder/adders.v
+++ half_adder/adders.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09.03.2024 11:44:08
-// Design Name: 
-// Module Name: adders
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module halfadder(
     input A,
